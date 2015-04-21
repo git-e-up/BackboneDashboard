fea892832c6d08cee60fb5c0fa72412ec1d8fab9
Matt Lissner <mlissne@gmail.com>
2015-04-20 19:05:12 -0500

    hmwrk

--- app.rb
+++ app.rb
@@ -1,6 +1,68 @@
 require 'bundler'
 Bundler.require()
 
+ActiveRecord::Base.establish_connection(
+  :adapter  => "postgresql",
+  :database => "carddata"
+)
+
+require './models/cards.rb'
+
+
 get '/' do
   erb :index
 end
+
+
+get '/api/cards' do
+  content_type :json
+  cards = Card.all
+  cards.to_json
+end
+
+
+get '/api/cards/:id' do
+  content_type :json
+  card = Card.find(params[:id].to_i)
+  card.to_json
+end
+
+post '/api/cards' do
+  content_type :json
+  card = Card.create(params[:card])
+  card.to_json
+end
+# Create a new card
+
+put '/api/cards/:id' do
+  content_type :json
+  card = Card.find(params[:id].to_i)
+  card.update(params[:card])
+  card.to_json
+end
+
+# Update an existing card
+
+patch '/api/cards/:id' do
+  content_type :json
+  card = Card.find(params[:id].to_i)
+  card.update(params[:card])
+  card.to_json
+  end
+# Update an existing card
+
+delete '/api/cards/:id' do
+  content_type :json
+  card = Card.delete(params[:id].to_i)
+  card.to_json
+  end
+# Delete an existing card
+
+
+
+
+
+# CREATE DATABASE carddata;
+
+
+# carddata=# CREATE TABLE cards (id SERIAL PRIMARY KEY, title VARCHAR(255), message VARCHAR(255));
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(event) {
 
-  var halfCardView = Backbone.View.extend({
+  var thirdOfACardView = Backbone.View.extend({
 
     tagName: "article",
     className: "block-third shadow",
@@ -16,13 +16,36 @@ $(document).ready(function(event) {
     }
   });
 
-  var data = { title: "Wenge", content: "Is a colour"}
-  var newCard = new halfCardView(data);
 
-  var dataTwo = { title: "Alice", content: "is Alice"};
-  var anotherCard = new halfCardView(dataTwo);
+  var halfOfACardView = Backbone.View.extend({
 
-  var dataThree = { title: "Bring me a shrubbery", content: <img src="https://www.youtube.com/embed/PDZcqBgCS74"></iframe>};
-  var anotherCard = new halfCardView(dataThree);
+    tagName: "article",
+    className: "block-half shadow",
+    template: _.template("<h3><%= title %></h3><%= content %><span class='close'>(x) dismiss</span>"),
+    initialize: function(data) {
+      this.render(data);
+    },
+    render: function(data) {
+      this.$el.html(this.template(data));
+      //$('body').append(this.$el);
+      var row = $('.row')[2];
+      $(row).append(this.$el);
+    }
+  });
+
+  var data = { title: '<h3 style="color: brown;">Wenge</h3>', content: '<article style="color: brown;">Is a colour</article>'}
+  var newCard = new thirdOfACardView(data);
+
+  var dataTwo = { title: "Alice", content: '<article style="border-style: solid;">Is Alice</article>'};
+  var anotherCard = new thirdOfACardView(dataTwo);
+
+  var dataThree = { title: "Bring me a shrubbery", content: '<img width="140" height="200" src="http://fc05.deviantart.net/fs71/i/2012/116/b/c/shrubbery_mega_pack_6_by_graphicpastock-d4xp957.jpg"></img> oh it\'s been brought\'n'};
+  var thirdCard = new thirdOfACardView(dataThree);
+
+  var dataFour = { title: '<h3 style="font-family: courier; margin-left:30px;">Matt</h3>', content: "is short for Matthew, but long on style!"};
+  var fourthCard = new halfOfACardView(dataFour);
+
+  var dataFive = { title: "This", content: '<article style="background-color: red;">Is also a card</article>'};
+  var fifthCard = new halfOfACardView(dataFive);
 
 });
